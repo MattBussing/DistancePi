d93b190670bda73918ba407e04904f1487106f09
MattBussing <mbussing@mymail.mines.edu>
2018-08-13 03:25:10 -0600

    added sleep to pi

--- code/main.py
+++ code/main.py
@@ -1,40 +1,66 @@
 # author Matt Bussing
+from time import sleep
 from messages import Messages
 from repeat import Repeat
-import sys
-
-URL='https://distance-pi.herokuapp.com/Matt'
+import sys, pytz
+from datetime import datetime, time
 
 
 if __name__ == '__main__':
+    URL='https://distance-pi.herokuapp.com/Matt'
+    morning = time(hour=9)
+    evening = time(hour=21)
     debug = False
+
     for i in sys.argv[1:]:
         if(i in "-d"):
             debug = True
-            URL='http://127.0.0.1:5000/Matt'
+            # URL='http://127.0.0.1:5000/Matt'
 
     m = Messages(URL)
+
+    # This is the process that updates the messages
     rep = Repeat(30, m.getMessages)
 
+    # This is the process that displays the information
     if debug:
         print("debug mode")
         rep2 = Repeat(3, m.display, print)
     else:
         import sense_hat
+        global sense
         sense = sense_hat.SenseHat()
         rep2 = Repeat(3, m.display, sense.show_message)
 
-    # Starts processes
+    # Starts the processes
     rep.start()
     rep2.start()
-    x = input("Hit enter to stop\n")
 
-    # Stops processes
-    rep.stop()
-    rep.join()
-    rep2.stop()
-    rep2.join()
+    # Loops until time for bed then it goes to sleep till morning
+    flag = False # flags if the process stops
+    while True:
+        rn = datetime.now(tz=pytz.utc).astimezone(pytz.timezone("America/Denver")).time()
+        bedTime = not(rn < evening and rn > morning)
+        print(bedTime)
+        if(bedTime):
+            # Stops processes
+            rep.stop()
+            rep.join()
+            rep2.stop()
+            rep2.join()
+            print("processes killed")
+            flag = True # sets flag
+            # waits 30 min
+            sleep(30*60)
 
+            if debug:
+                sense.clear()
+
+        elif(flag): # restarts processes if time to display
+            rep.start()
+            rep2.start()
+
+        sleep(5)
 
 # def printTime(x):
 #     print (x.strftime('%I:%M %p'))
--- code/messages.py
+++ code/messages.py
@@ -1,9 +1,6 @@
-import pytz, requests
-from datetime import datetime, time
+import requests
 
 class Messages():
-    morning = time(hour=9)
-    evening = time(hour=21)
     def __init__(self, url):
         # Stores a list of messages
         self.messageList = ["Message not updated yet"]
@@ -23,8 +20,5 @@ class Messages():
             self.messageList = ["Server not working properly"]
 
     def display(self, displayFunction):
-        rn = datetime.now(tz=pytz.utc).astimezone(pytz.timezone("America/Denver")).time()
-        if(rn < evening and rn > morning):
-            displayFunction("time for bed")
         for i in self.messageList:
             displayFunction(i)
