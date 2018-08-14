2b1a0082475c8e961653f27d31ec6adebb51ff60
MattBussing <mbussing@mymail.mines.edu>
2018-08-13 19:05:20 -0600

    fixed string error

--- code/messages.py
+++ code/messages.py
@@ -19,18 +19,20 @@ class Messages():
         if(r.status_code == 200):
             self.messageList = []
 
-            for i in r.json()['messages']:
-                local_tz = pytz.timezone("America/Denver")
-                postDate = datetime.strptime(i['dateTime'], "%Y-%m-%dT%H:%M:%S.%f") #2018-08-13T13:35:58.078103
-                postDate = local_tz.localize(postDate)
-                diff = datetime.now(tz=pytz.utc).astimezone(local_tz) - postDate
-                print(diff.total_seconds() , self.expirationDate)
+            mList = r.json()['messages']
+            if not(mList[0] == 'none'):
+                for i in mList:
+                    local_tz = pytz.timezone("America/Denver")
+                    postDate = datetime.strptime(i['dateTime'], "%Y-%m-%dT%H:%M:%S.%f") #2018-08-13T13:35:58.078103
+                    postDate = local_tz.localize(postDate)
+                    diff = datetime.now(tz=pytz.utc).astimezone(local_tz) - postDate
+                    print(diff.total_seconds() , self.expirationDate)
 
-                if diff.total_seconds() > self.expirationDate:
-                    self.DeleteMessages(i['message'])
-                    continue
+                    if diff.total_seconds() > self.expirationDate:
+                        self.DeleteMessages(i['message'])
+                        continue
 
-                self.messageList.append(i['message'])
+                    self.messageList.append(i['message'])
 
         else:
             self.messageList = ["Server not working properly"]
