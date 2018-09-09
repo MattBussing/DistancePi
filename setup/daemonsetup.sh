f6ca6175410a42b35e5462e6b3b5bb9b734cd6d8
MattBussing <mbussing@mymail.mines.edu>
2018-09-09 15:09:31 -0600

    fixed error

--- setup/daemonsetup.sh
+++ setup/daemonsetup.sh
@@ -1,6 +1,6 @@
 # https://www.raspberrypi-spy.co.uk/2015/10/how-to-autorun-a-python-script-on-boot-using-systemd/
 
 sudo ln -sf $HOME/DistancePi/setup/DistancePi.service /lib/systemd/system/
-sudo chmod 644 /lib/systemd/system/myscript.service
+sudo chmod 644 /lib/systemd/system/DistancPi.service
 sudo systemctl daemon-reload
 sudo systemctl enable myscript.service
