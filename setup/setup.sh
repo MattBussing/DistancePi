eab7a90c6898fa776e6967fd99fd488eed2cb4d2
MattBussing <mbussing@mymail.mines.edu>
2018-09-09 15:20:15 -0600

    updated rc.local

--- setup/setup.sh
+++ setup/setup.sh
@@ -33,6 +33,8 @@ echo '{
     "URL":"https://distance-pi.herokuapp.com",
     "EXPIRATION": 18000
 }' > code/config.json
+<<notes
+
 # checks to see if there is a back up for the file
 if [ ! -f /etc/rclocalBackup.txt ]; then
     sudo cp /etc/rc.local /etc/rclocalBackup.txt
@@ -40,9 +42,10 @@ fi
 sudo cp setup/rc.local /etc/rc.local
 sudo chmod +x /etc/rc.local
 
-
 sudo systemctl daemon-reload
 
 sudo reboot
 
 StandardOutput=tty
+sudo systemctl status myscript.service
+notes
