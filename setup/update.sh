8ee07656bbc18546b53ddd90e5c4ad575e891f81
MattBussing <mbussing@mymail.mines.edu>
2018-12-12 16:57:18 -0700

    added pip to updates

--- setup/update.sh
+++ setup/update.sh
@@ -4,4 +4,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo rpi-update
 sudo apt-get autoremove
 sudo apt-get autoclean
-sudo reboot
+
+# intstall and update programs
+sudo pip3 install -U pip
+sudo pip3 install -U pytz
+sudo pip3 install -U requests
