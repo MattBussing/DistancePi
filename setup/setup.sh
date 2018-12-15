31d6a7ed0b5ec9ca69cef1c8c8934a5151a1f83e
MattBussing <mbussing@mymail.mines.edu>
2018-12-15 10:25:22 -0700

    cleaned scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,32 +2,30 @@
 # set up ssh and wifi via raspi-config
 # for ssh add the file ssh to the boot folder
 
+# setup crontab with update.sh
 
-
+#install programs
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get install git
-sudo apt-get install python3-pip
-# sudo apt-get install vim
+sudo apt-get install git -y
+sudo apt-get install python3-pip -y
+sudo apt-get install vim -y
+sudo apt-get install sense-hat -y
+
+# gets repo
 cd
 if [ ! -d "DistancePi" ]; then
     sudo git clone https://github.com/MattBussing/DistancePi.git
 fi
 
+SCRIPTS=$HOME/DistancePi/setup/scripts
 # intstall and update programs
-sudo pip3 install -U pip
-sudo pip3 install pytz
-sudo pip3 install -U pytz
-sudo pip3 install requests
-sudo pip3 install -U requests
+bash $SCRIPTS/update.sh
 
-sudo apt-get install sense-hat
+bash $SCRIPTS/configSetup.sh
 
+bash $SCRIPTS/linkRC.sh
 
-bash /home/pi/DistancePi/setup/scripts/configSetup.sh
-
-bash /home/pi/DistancePi/setup/scripts/linkRC.sh
-
-bash $HOME/DistancePi/setup/scripts/daemonsetup.sh
+bash $SCRIPTS/daemonsetup.sh
 
 sudo reboot
