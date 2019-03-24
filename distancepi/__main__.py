0c98cf4b99ce93eacb52bf3a6a363ebe81c0a24f
MattBussing <mbussing@mymail.mines.edu>
2019-03-24 01:55:48 -0600

    got rid of debug prints

--- distancepi/__main__.py
+++ distancepi/__main__.py
@@ -34,9 +34,7 @@ class Device(object):
         self.sense_options()
 
     def load_config(self):
-        print(sys.argv)
         config_loc = get_args(sys.argv)
-        print(sys.argv)
         # loads config file (config)s
         try:
             with open(config_loc, 'r') as f:
@@ -253,7 +251,6 @@ def get_args(list):
     config_loc = None
     for i in range(1, length):
         if list[i] == "--config":
-            # print(list[i + 1][0:2])
             if length <= i + 1:
                 raise IOError("no config file specified")
             elif list[i + 1][0:2] == "--":
