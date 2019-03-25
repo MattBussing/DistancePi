6f8b0b106e2e43ceb8e1b49dfa21bcb2a8ec70b9
MattBussing <mbussing@mymail.mines.edu>
2019-03-25 01:11:20 -0600

    added more tests for add_thought_of

--- distancepi/device.py
+++ distancepi/device.py
@@ -136,7 +136,7 @@ class Device():
             then once we are more than eight add 1 to the
              far bottom left and so on
 
-                thought_of = 8
+                thought_of = 9
                 o, o, o, o, o, o, o, o,
                 o, p, p, o, p, p, o, o,
                 p, p, p, p, p, p, p, o,
@@ -144,21 +144,39 @@ class Device():
                 o, p, p, p, p, p, o, o,
                 o, o, p, p, p, o, o, o,
                 o, o, o, p, o, o, o, o,
-                o, o, o, o, o, o, o, g,
-                thought_of =
+                o, o, o, o, o, o, y, o,
+                thought_of = 10
                 o, o, o, o, o, o, o, o,
                 o, p, p, o, p, p, o, o,
                 p, p, p, p, p, p, p, o,
                 p, p, p, p, p, p, p, o,
                 o, p, p, p, p, p, o, o,
                 o, o, p, p, p, o, o, o,
-                o, o, o, p, o, o, o, g,
-                o, o, o, o, o, o, o, g,
+                o, o, o, p, o, o, o, o,
+                o, o, o, o, o, o, y, y,
+                thought_of = 18
+                o, o, o, o, o, o, o, o,
+                o, p, p, o, p, p, o, o,
+                p, p, p, p, p, p, p, o,
+                p, p, p, p, p, p, p, o,
+                o, p, p, p, p, p, o, o,
+                o, o, p, p, p, o, o, o,
+                o, o, o, p, o, o, o, o,
+                o, o, o, o, o, b, b, o,
+                thus 8*7+6 = 1 (0)
+                     8*7 +5 = 2 (0,1)
+                        8*7 +7 -i= (63 -i, 63)
+
+             what about at a really large number?
+
         """
         counter_color = Device.colors[int(
             self.model.thought_of / 9) % len(self.colors)]
         for i in range(0, self.model.thought_of % 9):
             heart[8 * (7 - i) + 7] = counter_color
+
+        for i in range(63 - int(self.model.thought_of / 9), 63):
+            heart[i] = counter_color
         return heart
 
     def display(self):
--- tests/test_device.py
+++ tests/test_device.py
@@ -161,9 +161,108 @@ def test_add_thought_of_9():
         o, p, p, p, p, p, o, o,
         o, o, p, p, p, o, o, o,
         o, o, o, p, o, o, o, o,
-        o, o, o, o, o, o, o, Device.yellow,
+        o, o, o, o, o, o, Device.yellow, o,
     ]
     d = Device()
     d.model.thought_of = 9
+    heart = d.add_thought_of(heart)
+    assert heart == goal
+
+
+def test_add_thought_of_10():
+    """
+    we want to start in the bottom right and work are way up
+    """
+    o = Device.nothing  # background
+    p = Device.red  # heart color
+
+    heart = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, o,
+        p, p, p, p, p, p, p, o,
+        p, p, p, p, p, p, p, o,
+        o, p, p, p, p, p, o, o,
+        o, o, p, p, p, o, o, o,
+        o, o, o, p, o, o, o, o,
+        o, o, o, o, o, o, o, o,
+    ]
+    goal = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, o,
+        p, p, p, p, p, p, p, o,
+        p, p, p, p, p, p, p, o,
+        o, p, p, p, p, p, o, o,
+        o, o, p, p, p, o, o, o,
+        o, o, o, p, o, o, o, o,
+        o, o, o, o, o, o, Device.yellow, Device.yellow,
+    ]
+    d = Device()
+    d.model.thought_of = 10
+    heart = d.add_thought_of(heart)
+    assert heart == goal
+
+
+def test_add_thought_of_16():
+    """
+    we want to start in the bottom right and work are way up
+    """
+    o = Device.nothing  # background
+    p = Device.red  # heart color
+
+    heart = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, o,
+        p, p, p, p, p, p, p, o,
+        p, p, p, p, p, p, p, o,
+        o, p, p, p, p, p, o, o,
+        o, o, p, p, p, o, o, o,
+        o, o, o, p, o, o, o, o,
+        o, o, o, o, o, o, o, o,
+    ]
+    goal = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, Device.yellow,
+        p, p, p, p, p, p, p, Device.yellow,
+        p, p, p, p, p, p, p, Device.yellow,
+        o, p, p, p, p, p, o, Device.yellow,
+        o, o, p, p, p, o, o, Device.yellow,
+        o, o, o, p, o, o, o, Device.yellow,
+        o, o, o, o, o, o, Device.yellow, Device.yellow,
+    ]
+    d = Device()
+    d.model.thought_of = 16
+    heart = d.add_thought_of(heart)
+    assert heart == goal
+
+
+def test_add_thought_of_18():
+    """
+    we want to start in the bottom right and work are way up
+    """
+    o = Device.nothing  # background
+    p = Device.red  # heart color
+
+    heart = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, o,
+        p, p, p, p, p, p, p, o,
+        p, p, p, p, p, p, p, o,
+        o, p, p, p, p, p, o, o,
+        o, o, p, p, p, o, o, o,
+        o, o, o, p, o, o, o, o,
+        o, o, o, o, o, o, o, o,
+    ]
+    goal = [
+        o, o, o, o, o, o, o, o,
+        o, p, p, o, p, p, o, o,
+        p, p, p, p, p, p, p, o,
+        p, p, p, p, p, p, p, o,
+        o, p, p, p, p, p, o, o,
+        o, o, p, p, p, o, o, o,
+        o, o, o, p, o, o, o, o,
+        o, o, o, o, o, Device.blue, Device.blue, o
+    ]
+    d = Device()
+    d.model.thought_of = 18
     heart = d.add_thought_of(heart)
     assert heart == goal
