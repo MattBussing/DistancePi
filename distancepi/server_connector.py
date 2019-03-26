4f7802d5b7dbb53565bcf6f5b19ebdb5673bf807
MattBussing <mbussing@mymail.mines.edu>
2019-03-25 22:34:14 -0600

    added count to get_data and removed get messages

--- distancepi/server_connector.py
+++ distancepi/server_connector.py
@@ -4,8 +4,6 @@ from datetime import datetime
 import pytz
 import requests
 
-from distancepi.models import Data
-
 
 class ServerConnector():
     """
@@ -21,37 +19,63 @@ class ServerConnector():
         message_list = list()
 
         if(r.status_code == 200):
-            data = Data(
-                self._get_messages(time_before=None)
-                self._get_thought_of())
+            data = r.json()
+            count = data['count']
+
+            # emptys the list
+            unparsed_message_list = data['messages']
+            # checks to see if anything passed
+            if not(unparsed_message_list is None):
+                # here it deletes old messages and adds the new
+                for i in unparsed_message_list:
+                    # returns the json string of date / time as a datetime
+                    # object with timezone
+                    # 2018-08-13T13:35:58.078103
+                    post_date = datetime.strptime(
+                        i['dateTime'], "%Y-%m-%dT%H:%M:%S.%f")
+                    post_date = pytz.utc.localize(post_date, is_dst=None)
+
+                    #  if time befoe is None we just add it to the list
+                    if time_before is not None and post_date < time_before:
+                        self._delete_messages(i['message'])
+                        continue
+
+                    # if it's not deleted, the message is added to the list
+                    message_list.append(i['message'])
+
+        else:
+            message_list = ["Server not working properly"]
+        return message_list, count
+
+    def get_messages(self, time_before=None):
+        r = requests.get(url=self.url + self.client)
+        message_list = list()
+
+        if(r.status_code == 200):
+            # emptys the list
+            unparsed_message_list = r.json()['messages']
+            # checks to see if anything passed
+            if not(unparsed_message_list[0] == 'none'):
+                # here it deletes old messages and adds the new
+                for i in unparsed_message_list:
+                    # returns the json string of date / time as a datetime
+                    # object with timezone
+                    # 2018-08-13T13:35:58.078103
+                    post_date = datetime.strptime(
+                        i['dateTime'], "%Y-%m-%dT%H:%M:%S.%f")
+                    post_date = pytz.utc.localize(post_date, is_dst=None)
+
+                    #  if time befoe is None we just add it to the list
+                    if time_before is not None and post_date < time_before:
+                        self._delete_messages(i['message'])
+                        continue
+
+                    # if it's not deleted, the message is added to the list
+                    message_list.append(i['message'])
+
         else:
             message_list = ["Server not working properly"]
         return message_list
-
-    def _get_messages(self, time_before=None):
-            # emptys the list
-        unparsed_message_list = r.json()['messages']
-        # checks to see if anything passed
-        if not(unparsed_message_list[0] == 'none'):
-            # here it deletes old messages and adds the new
-            for i in unparsed_message_list:
-                # returns the json string of date / time as a datetime
-                # object with timezone
-                # 2018-08-13T13:35:58.078103
-                post_date = datetime.strptime(
-                    i['dateTime'], "%Y-%m-%dT%H:%M:%S.%f")
-                post_date = pytz.utc.localize(post_date, is_dst=None)
-
-                #  if time befoe is None we just add it to the list
-                if time_before is not None and post_date < time_before:
-                    self._delete_messages(i['message'])
-                    continue
-
-                # if it's not deleted, the message is added to the list
-                message_list.append(i['message'])
-
-    def _get_thought_of(self):
-        pass
 
     def _delete_messages(self, message):
         requests.delete(url=self.url + self.client, json={'message': message})
